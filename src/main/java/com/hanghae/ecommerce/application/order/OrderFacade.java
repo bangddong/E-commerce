ea001d7c0dc423b7e9ebab41e2cb53946dca5402
dhbang <gusehdqkd@naver.com>
2024-10-31 22:48:38 +0900

    feat : 분산락 구현  - redisson을 활용하여 분산락 구현

--- src/main/java/com/hanghae/ecommerce/application/order/OrderFacade.java
+++ src/main/java/com/hanghae/ecommerce/application/order/OrderFacade.java
@@ -1,8 +1,5 @@
 package com.hanghae.ecommerce.application.order;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantLock;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,17 +23,6 @@ public class OrderFacade {
 	private final CartService cartService;
 	private final UserService userService;
 
-	private final ConcurrentHashMap<Long, ReentrantLock> locks = new ConcurrentHashMap<>();
-
-	private ReentrantLock getProductLock(long productId) {
-		return locks.computeIfAbsent(productId, key -> new ReentrantLock());
-	}
-
-	private ReentrantLock getUserLock(long userId) {
-		return locks.computeIfAbsent(userId, key -> new ReentrantLock());
-	}
-
-	@Transactional
 	public void createOrder(OrderCommand.CreateOrderRequest command) {
 		var cart = cartService.getCart(command.getCartId());
 		var cartItems = cart.getCartItems();
@@ -58,10 +44,7 @@ public class OrderFacade {
 			.sum();
 
 		var user = userService.getUser(cart.getUserId());
-		ReentrantLock userLock = getUserLock(user.getId());
-		userLock.lock();
 		userService.useAmount(user.getId(), totalAmount);
-		userLock.unlock();
 
 		orderService.createOrder(user.getId(), totalAmount);
 		cartItems.forEach(cartItem -> {
@@ -75,10 +58,7 @@ public class OrderFacade {
 		});
 
 		cart.getCartItems().forEach(cartItem -> {
-			ReentrantLock productLock = getProductLock(cartItem.getProductId());
-			productLock.lock();
 			productService.reduceStock(cartItem.getProductId(), cartItem.getQuantity());
-			productLock.unlock();
 		});
 
 		cartService.clearCart(cart.getId());
