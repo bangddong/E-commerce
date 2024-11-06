a6da0af3744cceff2cbe6ce4a9b60e24b5f05753
dhbang <gusehdqkd@naver.com>
2024-11-06 23:34:04 +0900

    test : 캐싱 테스트 추가

---
+++ src/test/java/com/hanghae/ecommerce/domain/product/ProductServiceCacheTest.java
@@ -0,0 +1,91 @@
+package com.hanghae.ecommerce.domain.product;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cache.CacheManager;
+
+@SpringBootTest
+class ProductServiceCacheTest {
+
+	@Autowired
+	private ProductServiceImpl productService;
+
+	@MockBean
+	private ProductReader productReader;
+
+	@Autowired
+	private CacheManager cacheManager;
+
+	@BeforeEach
+	void setUp() {
+		Objects.requireNonNull(cacheManager.getCache("productList")).clear();
+		Objects.requireNonNull(cacheManager.getCache("product")).clear();
+		Objects.requireNonNull(cacheManager.getCache("topSellingProducts")).clear();
+	}
+
+	@Test
+	void getProducts_CacheableTest() {
+		// Given
+		List<Product> mockProducts = List.of(
+			mock(Product.class)
+		);
+		when(productReader.getProducts()).thenReturn(mockProducts);
+
+		// When
+		List<ProductInfo.Main> result1 = productService.getProducts();
+		List<ProductInfo.Main> result2 = productService.getProducts();
+
+		// Then
+		verify(productReader, times(1)).getProducts();
+		assertThat(result1).usingRecursiveComparison().isEqualTo(result2);
+	}
+
+	@Test
+	void getProduct_CacheableTest() {
+		// Given
+		Long productId = 1L;
+		Product mockProduct= mock(Product.class);
+
+		when(productReader.getProduct(productId)).thenReturn(mockProduct);
+
+		// When
+		ProductInfo.Main result1 = productService.getProduct(productId);
+		ProductInfo.Main result2 = productService.getProduct(productId);
+
+		// Then
+		verify(productReader, times(1)).getProduct(productId);
+		assertThat(result1).usingRecursiveComparison().isEqualTo(result2);
+	}
+
+	@Test
+	void updateProduct_CacheEvictionTest() {
+		// Given
+		Long productId = 1L;
+		Product mockProduct = mock(Product.class);
+
+		when(productReader.getProduct(productId)).thenReturn(mockProduct);
+
+		// When & Then
+		productService.getProduct(productId);
+		verify(productReader, times(1)).getProduct(productId);
+
+		productService.getProduct(productId);
+		verify(productReader, times(1)).getProduct(productId);
+
+		Product mockProduct2 = mock(Product.class);
+		productService.updateProduct(mockProduct2);
+
+		productService.getProduct(productId);
+		verify(productReader, times(2)).getProduct(productId);
+	}
+
+}
